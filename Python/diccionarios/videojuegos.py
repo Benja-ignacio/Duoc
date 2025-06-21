efee0b197a6d0e99887b674362ee45593bbaa3cd
Benjamin <69690921+Benja-ignacio@users.noreply.github.com>
2025-06-21 19:15:01 -0400

    Update videojuegos.py
    w

--- Python/diccionarios/videojuegos.py
+++ Python/diccionarios/videojuegos.py
@@ -3,19 +3,26 @@ import re
 from time import sleep as pausa
 datos_videojuegos = {"juegos":[]}
 
+def limpiar():
+    import os
+    if os.name == 'nt':
+        os.system('cls')  # Windows
+    else:
+        os.system('clear')  # Linux/macOS
+
 def datos_repetidos(nuevo_juego):
     for i in datos_videojuegos["juegos"]:
         id_repetido = i['id'] == nuevo_juego['id']
         nombre_repetido = i["nombre"] == nuevo_juego['nombre']
 
         if id_repetido and nombre_repetido:
-            print(f"No se logro registrar el juego porque los suguientes datos se encuentran en uso: ID: {i["id"]}, Nombre: {i["nombre"]}\n")
+            print(f"\nNo se logro registrar el juego porque los suguientes datos se encuentran en uso: ID: {i["id"]}, Nombre: {i["nombre"]}\n")
             return True
         elif id_repetido:
-            print(f"No se logro registrar el juego porque el siguientes dato se encuentra en uso: {i["id"]}")
+            print(f"\nNo se logro registrar el juego porque el siguientes dato se encuentra en uso: {i["id"]}")
             return True
         elif nombre_repetido:
-            print(f"No se logro registrar el juego porque el siguiente dato se encuentra en uso: {i["id"]}")
+            print(f"\nNo se logro registrar el juego porque el siguiente dato se encuentra en uso: {i["id"]}")
             return True
     return False # Retorna falso si no se encontro nada repetido  
 
@@ -25,17 +32,21 @@ def validar_entero(mensaje:str, min=None, max=None)->int:
         try:
             valor = int(input(mensaje))
         except ValueError:
-            print("Debes ingresar un numero entero.")
+            limpiar()
+            print("Debes ingresar un numero entero.\n")
             continue
         if  min is not None and max is not None:
             if not min <= valor <= max:
-                print(f"El numero ingresado debe estar dentro del rango permitido. {(min,max)}")
+                limpiar()
+                print(f"El numero ingresado debe estar dentro del rango permitido. {(min,max)}\n")
                 continue
         if min is not None and valor < min:
-            print(f"Debe ser >= al minimo. (min = {min})")
+            limpiar()
+            print(f"Debe ser >= al minimo. (min = {min})\n")
             continue
         if max is not None and valor > max:
-            print(f"Debe ser <= al maximo. (max = ({max})")
+            limpiar()
+            print(f"Debe ser <= al maximo. (max = ({max})\n")
             continue
         return valor
 
@@ -57,9 +68,9 @@ def agregar_juego():
         }
         
         if datos_repetidos(nuevo_juego):
-            continue
+            break
         datos_videojuegos["juegos"].append(nuevo_juego)
-        print(f"El Videojuego {nuevo_juego["nombre"]} ha sido agregado correctamente!")
+        print(f"\nEl Videojuego {nuevo_juego["nombre"]} ha sido agregado correctamente!")
         input("Presione enter para volver al menu principal: ")
         break
 
@@ -73,6 +84,7 @@ def hay_datos():
 def mostrar_datos():
     if not hay_datos():
         return
+    limpiar()
     for i in datos_videojuegos["juegos"]:
         print(f"ID: {i['id']}")
         print(f"Nombre: {i['nombre']}")
@@ -91,8 +103,9 @@ def buscar_juego()->str:
                 print(f"ID: {i["id"]}, Nombre: {i["nombre"]}, Plataforma: {i["plataforma"]}, Año: {i["anio"]}")
                 input("\nPresione enter para volver al menu: ")
                 return
-        print("No se encontro ningun juego con el nombre ingresado.")
-        continue
+        print("\nNo se encontro ningun juego con el nombre ingresado. Volviendo al menu principal...")
+        pausa(2)
+        break
 
 def eliminar_juego():
     if not hay_datos():
@@ -108,24 +121,52 @@ def eliminar_juego():
         print("No se encontro ningun juego con el id ingresado.")
         continue
 
+def modificar_juego():
+    if not hay_datos():
+        return
+    while True:
+        id_juego = validar_string("Ingrese el id del juego que desea modificar: ", r"^[a-zA-Z\d]{6}")
+        for i in datos_videojuegos["juegos"]:
+            if i['id'] == id_juego:
+                opcion = validar_entero("\n1. Modificar Nombre\n2. Modificar Plataforma\n3. Modificar año\n4. Salir\n: ", 1, 4)
+                if opcion == 1:
+                    modificar_nombre = validar_string("Ingrese el nuevo nombre: ", r"^[a-zA-Z\d:. ]{3,64}").capitalize()
+                    i["nombre"] = modificar_nombre
+                    input("El nombre se ha modificado correctamente!\nPresione enter para volver al menu: ")
+                    
+                if opcion == 2:
+                    modificar_plataforma = validar_string("Ingrese la nueva plataforma del juego: ", r"^[a-zA-Z :.\d]{3,64}").capitalize()
+                    i["plataforma"] = modificar_plataforma
+                    input("La plataforma se ha modificado correctamente!\nPresione enter para volver al menu: ")
+        
+                if opcion == 3:
+                    modificar_anio =validar_entero("Ingrese el nuevo año de de salida del juego: ", 1950, 2025)
+                    i["anio"] = modificar_anio 
+                    input("El año se ha modificado correctamente!\nPresione enter para volver al menu: ")
+                    
+                if opcion == "4":
+                    return
+        print("No se encontro ningun juego con el id ingresado.\nVolviendo al menu principal...")    
+        pausa(1.5)
+        break
 while True:
-    print("***Administrador de juegos***")
-    try:
-        opciones = int(input("1. Agregar Juegos\n2. Listar los juegos\n3. Buscar por nombre\n4. Eliminar por id\n5. Salir\n: "))
-    except ValueError:
-        print("Debes ingresar una opcion valida. (1-5)")
-        continue
-    if not 1 <= opciones <= 5:
-        print("Debes ingresar una opcion valida. (1-5)")
-        continue
+    limpiar()
+    print("  ***Administrador de juegos***\n")
+    opciones = validar_entero("1. Agregar Juegos\n2. Listar los juegos\n3. Buscar por nombre\n4. Eliminar por id\n5. Modificar juego\n6. Salir\n: ", 1,6)
     if opciones == 1:
         agregar_juego()
     elif opciones == 2:
+        limpiar()
         mostrar_datos()
     elif opciones == 3:
+        limpiar()
         buscar_juego()
     elif opciones == 4:
+        limpiar()
         eliminar_juego()
     elif opciones == 5:
+        limpiar()
+        modificar_juego()
+    elif opciones == 6:
         break
 
