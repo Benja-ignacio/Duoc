73b81cfa4f5bd8633104834032e9b991d018adf1
Benjamin Ignacio <69690921+Benja-ignacio@users.noreply.github.com>
2025-08-14 12:32:03 -0400

    asd

--- matematica-aplicada/test.py
+++ matematica-aplicada/test.py
@@ -1,30 +1,18 @@
 estudiantes = [16.43, 19.31, 10.25, 18.63, 17.85, 19.76, 26.64, 21.94,21.3, 22.67, 16.48]
 
-bajo_peso = []
-peso_normal = []
-sobre_peso = []
-obesidad = []
-
-categorias = [bajo_peso, peso_normal, sobre_peso, obesidad]
-
+categorias = ["Bajo peso", "Peso normal", "Sobrepeso", "Obesidad"]
 
 def categorias_imc(imc):
         if imc < 18.5:
-            bajo_peso.append(imc)
+            return categorias[0]
         elif 18.5 <= imc <= 24.9:
-            peso_normal.append(imc)
+            return categorias[1] 
         elif 25 <= imc <= 29.9:
-            sobre_peso.append(imc)
+            return categorias[2]
         else:
-            obesidad.append(imc)
+            return categorias[3]
 
-# contador = 0
 
-# for i in estudiantes:
-#     categorias_imc(i)
-
-# print(f"Estudiante numero: {contador} Sobrepeso: {sobre_peso}"
-
-for indice, valor in enumerate(estudiantes):
-    print(f"Estudiante {indice}: IMC {valor} Categoria: {categorias}")
-    categorias_imc(valor)
+for indice, valor in enumerate(estudiantes, start=1):
+    categoria = categorias_imc(valor)
+    print(f"Estudiante {indice}: IMC {valor} Categoria {categoria}")
